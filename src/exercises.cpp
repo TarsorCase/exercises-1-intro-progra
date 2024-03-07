61607c5b3b569ab94cf5871e6a9d47c7b86803bb
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-03-07 14:19:06 -0400

    cambios 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -52,10 +52,10 @@ void exercise_1(string s1, string s2, string s3) {
             }
             cout << minimum_line;
         }else {
-        cout << "Una de las oraciones o palabras excede los 30 caracteres permitidos";
+        
         }
     }else {
-        cout << "No todos los caracteres son minusculas." << endl;
+        
     }
 }
 

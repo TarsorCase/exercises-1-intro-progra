826265dd57f4cc4c0ab74625f653b87c71e78705
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-03-08 07:50:23 -0400

    Cambios hechos al 7

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -173,7 +173,7 @@ void exercise_7(double r) {
    }else {
         double power = pow(r, 2);
         double area = 4 * M_PI * power;
-        cout << area;
+        cout << fixed << setprecision(6) << area << "\n";
    }
 }
 

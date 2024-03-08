edb1d5ab2b2aec7ba61a1d36165925809cfb6f66
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-03-08 19:40:49 -0400

    Ejercicio 10

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -248,7 +248,12 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 }
 
 int exercise_10(int a, int b) {
-  // TODO: YOUR CODE HERE
+  int sum_num = a + b;
+    cout << sum_num << "\n";
+    string size_num_string = to_string(sum_num);
+    int size_num = size_num_string.size();
+    int product = size_num * sum_num;
+    cout << product << "\n";
 }
 
 string exercise_11(int number) {

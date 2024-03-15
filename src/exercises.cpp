9e02f2bcec8b167b4b53000fee1c9a8161aa79a8
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-03-15 13:50:38 -0400

    Haber ejercficio 14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -315,7 +315,8 @@ string exercise_13(int age, int years_of_experience) {
 
 string exercise_14(int number_of_docs) {
     if (number_of_docs > 1){
-        return number_of_docs " documentos encontrados.";
+        string docs = to_string(number_of_docs) + " documentos encontrados.";
+        return docs;
     }else if(number_of_docs == 0){
         return "No se encontraron documentos.";
     }else {
